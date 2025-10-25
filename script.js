074fca94dff8d9f6a2da54a5dd537803186a333c
dunt21 <suadmacaulay50@gmail.com>
2025-10-25 23:58:18 +0000

    feat: improve form layout and enhance error handling in age calculator

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const dayContainer = document.querySelector(".dayField");
 const monthContainer = document.querySelector(".monthField");
 const yearContainer = document.querySelector(".yearField");
 const allInputs = document.querySelectorAll("input");
+let hasErr;
 
 // const errMsg = document.
 // const errMsg = document.createElement("p");
@@ -18,13 +19,13 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   validateInputs();
+  calculateAge();
 });
 
 function createErrMsg(msg, id) {
-  uiErr();
-
+  uiErr("add");
+  hasErr = true;
   const errMsg = document.createElement("p");
-
   errMsg.textContent = msg;
   errMsg.id = id;
   errMsg.classList.add("textErr", "italic", "text-xs", "mt-2");
@@ -32,24 +33,66 @@ function createErrMsg(msg, id) {
   return errMsg;
 }
 
-//to display an err when the field is empty
-function checkEmptyField() {
+//to check for the various errors in any input field
+function checkErrs() {
+  const inputValues = [];
+
   allInputs.forEach((inp) => {
     const parentEl = inp.parentElement;
-    const existingMSg = parentEl.querySelector("p");
-    existingMSg?.classList.remove("hidden");
+    const emptyErr = parentEl.querySelector("#empty-field-err");
+    const dayErr = parentEl.querySelector(`#invalid-day`);
+    const monthErr = parentEl.querySelector(`#invalid-month`);
+    const yearErr = parentEl.querySelector(`#invalid-year`);
+
+    parentEl.querySelectorAll("p").forEach((p) => p.classList.remove("hidden"));
     const inputId = inp.id;
 
-    inp.value === "" && !existingMSg
+    inp.value === "" && !emptyErr
       ? parentEl.appendChild(
           createErrMsg("This field is required", "empty-field-err")
         )
       : removeErrMsg();
 
-    inputId === "day" && inp.value > 31 && !existingMSg
-      ? parentEl.appendChild(createErrMsg("Must be a valid day", "invalid-day"))
-      : removeErrMsg();
+    if (inputId === "day" && inp.value > 31) {
+      uiErr("add");
+
+      // const invalidDateErr = dayContainer.querySelector("#invalid-date");
+      // if (invalidDateErr) return;
+
+      console.log("wow");
+      if (!dayErr)
+        parentEl.appendChild(
+          createErrMsg("Must be a valid day", "invalid-day")
+        );
+    } else {
+      dayErr?.classList.add("hidden");
+    }
+
+    if (inputId === "month" && inp.value > 12) {
+      uiErr("add");
+      if (!monthErr)
+        parentEl.appendChild(
+          createErrMsg("Must be a valid month", "invalid-month")
+        );
+    } else {
+      monthErr?.classList.add("hidden");
+    }
+
+    if (inputId === "year" && inp.value > new Date().getFullYear()) {
+      uiErr("add");
+
+      if (!yearErr)
+        parentEl.appendChild(
+          createErrMsg("Must be a valid year", "invalid-year")
+        );
+    } else {
+      yearErr?.classList.add("hidden");
+    }
+
+    inputValues.push(+inp.value);
   });
+
+  return inputValues;
 }
 
 //remove error msg
@@ -57,54 +100,73 @@ function removeErrMsg() {
   allInputs.forEach((inp) => {
     if (inp.value !== "") {
       const parentEl = inp.parentElement;
-      const existingMSg = parentEl.querySelector("p");
+      const existingMSg = parentEl.querySelector(`#empty-field-err`);
       existingMSg?.classList.add("hidden");
     }
   });
 }
 
 //to display an err for the label and input
-function uiErr() {
+function uiErr(action) {
   document
     .querySelectorAll("label")
-    .forEach((label) => label.classList.add("textErr"));
+    .forEach((label) => label.classList[action]("textErr"));
 
-  allInputs.forEach((inp) => inp.classList.add("inputErr"));
+  allInputs.forEach((inp) => inp.classList[action]("inputErr"));
 }
 
 function validateInputs() {
-  checkEmptyField();
+  const values = checkErrs();
+  const invalidDateErr = dayContainer.querySelector("#invalid-date");
 
-  // if (inputDay.value > 31 && !inputDay.parentElement.querySelector("p")) {
-  //   inputDay.parentElement.appendChild(
-  //     createErrMsg("Must be a valid day", "invalinod-day")
-  //   );
-  // } else {
-  //   inputDay.parentElement
-  //     .querySelector("#invalid-day")
-  //     ?.classList.add("hidden");
-  // }
+  if (values.some((val) => val === 0)) return;
 
-  // if (inputMonth.value > 12 && !inputMonth.parentElement.querySelector("p")) {
-  //   inputMonth.parentElement.appendChild(
-  //     createErrMsg("Must be a valid month", "invalid-month")
-  //   );
-  // } else {
-  //   inputMonth.parentElement
-  //     .querySelector("#invalid-month")
-  //     ?.classList.add("hidden");
-  // }
+  const monthDays = new Date(0, values[1], 0).getDate();
 
-  // if (
-  //   inputYear.value > new Date().getFullYear() &&
-  //   !inputYear.parentElement.querySelector("p")
-  // ) {
-  //   inputYear.parentElement.appendChild(
-  //     createErrMsg("Must be in the past", "invalid-year")
-  //   );
-  // } else {
-  //   inputYear.parentElement
-  //     .querySelector("#invalid-year")
-  //     ?.classList.add("hidden");
-  // }
+  if (values[0] > monthDays) {
+    uiErr("add");
+
+    dayContainer.querySelector(`#invalid-day`).classList.add("hidden");
+
+    if (!invalidDateErr)
+      inputDay.insertAdjacentElement(
+        "afterend",
+        createErrMsg("Must be a valid date", "invalid-date")
+      );
+    console.log("date is invalid");
+  } else {
+    invalidDateErr?.classList.add("hidden");
+  }
+
+  // console.log(values);
+  return values;
+}
+
+function calculateAge() {
+  const dateNow = new Date();
+  console.log(dateNow);
+
+  const vals = validateInputs();
+
+  if (!vals) return;
+  uiErr("remove");
+
+  console.log(dateNow.getDate());
+  console.log(1 - dateNow.getFullYear());
+
+  const days = vals[0] - dateNow.getDate();
+  const month = vals[1] - dateNow.getMonth() - 1;
+  const year = vals[2] - dateNow.getFullYear();
+
+  const displayDates = document
+    .querySelector(".age-display")
+    .querySelectorAll("span");
+
+  displayDates.forEach((p) => {
+    if (p.classList.contains("display-day")) p.textContent = Math.abs(days);
+    if (p.classList.contains("display-month")) p.textContent = Math.abs(month);
+    if (p.classList.contains("display-year")) p.textContent = Math.abs(year);
+  });
+
+  console.log(vals);
 }
